897d145295312a2838b96238c537418cd42f84f0
Joe <joechu0510@gmail.com>
2026-02-17 23:00:35 -0800

    Update convert_to_qnn.sh

--- convert_to_qnn.sh
+++ convert_to_qnn.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-echo "usage: $0 <path-to-pytorch-model> <batch num> <channels> <height> <width> <beackend>"
+echo "usage: $0 <path-to-pytorch-model> <batch num> <channels> <height> <width> <beackend> <htp version>"
 
 MODEL_PATH=$1
 BATCH_NUM=$2
@@ -9,6 +9,7 @@ CHANNELS=$3
 HEIGHT=$4
 WIDTH=$5
 BACKEND=$6
+HTP_VERSION=${7:-v73}
 
 rm -rf model_tmp
 mkdir -p model_tmp
@@ -19,10 +20,10 @@ if [ "$BACKEND" = "cpu" ] || [ "$BACKEND" = "gpu" ]; then
     echo "Converting Pytorch model to FP32 QNN model"
 
     TORCH_MODEL_NAME=$(basename "$MODEL_PATH" .pt)
-    QNN_MODEl_NAME="${TORCH_MODEL_NAME}_fp32"
+    QNN_MODEL_NAME="${TORCH_MODEL_NAME}_fp32"
 
-    QNN_MODEL_CPP_NAME="${QNN_MODEl_NAME}.cpp"
-    QNN_MODEL_BIN_NAME="${QNN_MODEl_NAME}.bin"
+    QNN_MODEL_CPP_NAME="${QNN_MODEL_NAME}.cpp"
+    QNN_MODEL_BIN_NAME="${QNN_MODEL_NAME}.bin"
 
     python3 "${QNN_SDK_ROOT}/bin/x86_64-linux-clang/qnn-pytorch-converter" \
     --input_network "${MODEL_PATH}" \
@@ -38,6 +39,64 @@ if [ "$BACKEND" = "cpu" ] || [ "$BACKEND" = "gpu" ]; then
 
 elif [ "$BACKEND" = "htp" ]; then
     echo "Converting Pytorch model to Quantized QNN model"
+
+    TORCH_MODEL_NAME=$(basename "$MODEL_PATH" .pt)
+    QNN_MODEL_NAME="${TORCH_MODEL_NAME}_quant"
+
+    QNN_MODEL_CPP_NAME="${QNN_MODEL_NAME}.cpp"
+    QNN_MODEL_BIN_NAME="${QNN_MODEL_NAME}.bin"
+
+    MODEL_DIR=$(dirname "$MODEL_PATH")
+    echo "${MODEL_DIR}/test.raw" > ./model_tmp/input_list_quant.txt
+
+
+    {
+      echo '{'
+      echo '  "graphs": ['
+      echo '    {'
+      echo '      "graph_names": ['
+      echo "        \"${QNN_MODEL_NAME}\""
+      echo '      ],'
+      echo '      "vtcm_mb": 2'
+      echo '    }'
+      echo '  ],'
+      echo '  "devices": ['
+      echo '    {'
+      echo "      \"htp_arch\": \"${HTP_VERSION}\""
+      echo '    }'
+      echo '  ]'
+      echo '}'
+    } > ./model_tmp/htp_config.json
+
+    {
+      echo '{'
+      echo '  "backend_extensions": {'
+      echo "    \"shared_library_path\": \"${QNN_SDK_ROOT}/lib/${QNN_TARGET_ARCH}/libQnnHtpNetRunExtensions.so\","
+      echo '    "config_file_path": "./model_tmp/htp_config.json"'
+      echo '  }'
+      echo '}'
+    } > ./model_tmp/htp_backend_extensions.json
+    
+
+    python3 "${QNN_SDK_ROOT}/bin/x86_64-linux-clang/qnn-pytorch-converter" \
+    --input_network "${MODEL_PATH}" \
+    --input_dim input $BATCH_NUM,$CHANNELS,$HEIGHT,$WIDTH \
+    -o ./model_tmp/$QNN_MODEL_CPP_NAME \
+    --input_list ./model_tmp/input_list_quant.txt
+
+    python3 "${QNN_SDK_ROOT}/bin/x86_64-linux-clang/qnn-model-lib-generator" \
+    -c ./model_tmp/${QNN_MODEL_CPP_NAME} \
+    -b ./model_tmp/${QNN_MODEL_BIN_NAME} \
+    -o ./model_tmp/quant \
+    -t $QNN_TARGET_ARCH
+
+    ${QNN_SDK_ROOT}/bin/${QNN_TARGET_ARCH}/qnn-context-binary-generator \
+    --backend "${QNN_SDK_ROOT}/lib/${QNN_TARGET_ARCH}/libQnnHtp.so" \
+    --model "./model_tmp/quant/${QNN_TARGET_ARCH}/lib${QNN_MODEL_NAME}.so" \
+    --binary_file "./model_tmp/${QNN_MODEL_NAME}.serialized" \
+    --config_file "./model_tmp/htp_backend_extensions.json"
+
+
 else
     echo "Unsupported backend: $BACKEND"
     exit 1
