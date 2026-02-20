a321baf135bf62f02466edab25d57c5bcd116adc
Joe <joechu0510@gmail.com>
2026-02-20 10:39:52 -0800

    Add qnn commons

--- qnnx/include/Loader.h
+++ qnnx/include/Loader.h
@@ -1,10 +1,25 @@
 #pragma once
 
+#include <dlfcn.h>
+#include <stdlib.h>
+
+#include <string>
+
 #include "QNNCommons.h"
 
 namespace qnnx {
 
-QNNResults GetQNNFunctionPointer();
+void dlOpen(const char* filename, int flags);
+
+void dlSym(void* handle, const char* symbol);
+
+void dlClose(void* handle);
+
+char dlError();
+
+QNNResults GetQNNFunctionPointer(std::string backend_path, std::string model_path,
+                                 QnnFunctionPointers* function_pointer, void** backend_handle,
+                                 bool load_model_lib, void** model_handle);
 
 QNNResults GetQNNSystemPointer();
 
---
+++ qnnx/src/Loader.cpp
@@ -0,0 +1,14 @@
+#include "Loader.h"
+
+namespace qnnx {
+
+// QNNResults GetQNNFunctionPointer(std::string backend_path,
+//                                   std::string model_path,
+//                                   QnnFunctionPointers* function_pointer,
+//                                   void** backend_handle,
+//                                   bool load_model_lib,
+//                                   void** model_handle) {
+
+// }
+
+}  // namespace qnnx
