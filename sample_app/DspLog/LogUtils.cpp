4d4c8dc5ed142883ddc75e567d9d33a5cd4e61d7
Joe <joechu0510@gmail.com>
2026-02-20 21:30:40 -0800

    add files

---
+++ sample_app/DspLog/LogUtils.cpp
@@ -0,0 +1,21 @@
+//==============================================================================
+//
+//  Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+//  All rights reserved.
+//  Confidential and Proprietary - Qualcomm Technologies, Inc.
+//
+//==============================================================================
+
+#include "LogUtils.hpp"
+
+#include "HAP_farf.h"
+
+#define PRINTLEN 1024
+
+void qnn::log::utils::logDefaultCallback(const char* fmt, QnnLog_Level_t level, uint64_t timestamp,
+                                         va_list argp) {
+  char buffer[PRINTLEN] = "";
+  std::lock_guard<std::mutex> lock(sg_logUtilMutex);
+  vsnprintf(buffer, sizeof(buffer), fmt, argp);
+  FARF(ALWAYS, "[%x] %s", level, buffer);
+}
---
+++ sample_app/Utils/BuildId.hpp
@@ -0,0 +1,17 @@
+//==============================================================================
+//
+//  Copyright (c) 2020, 2024 Qualcomm Technologies, Inc.
+//  All Rights Reserved.
+//  Confidential and Proprietary - Qualcomm Technologies, Inc.
+//
+//==============================================================================
+
+#pragma once
+
+namespace qnn {
+namespace tools {
+
+inline std::string getBuildId() { return std::string("v2.43.0.260127150333_193827"); }
+
+}  // namespace tools
+}  // namespace qnn
